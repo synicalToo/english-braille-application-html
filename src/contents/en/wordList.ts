7af5e7ba94101a1dedb8145701e70388de7b33f8
synical <87197716+synicalToo@users.noreply.github.com>
2024-11-27 11:32:07 +0900

    add countdown timer and basic replication of gameplay screen

---
+++ src/contents/en/wordList.ts
@@ -0,0 +1,1 @@
+export const WordList = ["apple", "banana", "cherry", "date", "big", "small", "boy", "girl", "fun", "run", "hide", "seek", "jump", "play", "stop", "fast", "slow", "cat", "dog", "sun", "moon", "star", "sky"];
